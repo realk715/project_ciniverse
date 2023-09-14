6dce6f09a30aaedcc5202d19be075b38ba531e54
realkk715 <realkk715@gmail.com>
2023-09-14 07:22:25 +0700

    fix id params in movie and install debug

--- project_cinirverse_frontend/src/app/movie/[id]/page.tsx
+++ project_cinirverse_frontend/src/app/movie/[id]/page.tsx
@@ -5,7 +5,7 @@ type Props = {
 }
 
 export default  async function page({params}: Props) {
-  const { id } = params;
+  const id = params;
   const img ="https://image.tmdb.org/t/p/original";
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=72eef7bc29be08e73392ec8cc0b64c52`;
   const response = await fetch(url,{next :{revalidate:60}}); //ครบ60วิ clear cache 
